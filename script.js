112be6b390679cee2143e4630194f9188bc2a830
floff02 <102332546+floff02@users.noreply.github.com>
2022-03-24 23:58:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.body.style.background = "#EEE5D8"; //sets background colour
+
+document.getElementsByClassName('PD')[0].style.backgroundColor = "rgb(239, 225, 204)"; //gets element using class name to change the background colour of a specific division
+
+document.getElementsByClassName('footer')[0].style.backgroundColor = "rgb(80, 35, 20)"; //same as above for different division
